de543581451f6e05707a4bc1b5837a1a9bf3b212
Zhijie He <hezhijie0327@hotmail.com>
2020-07-25 17:13:37 +0800

    release.sh | Version 1.0.2
    1. <Change> Rollback "multi-upstream" test.

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Current Version: 1.0.1
+# Current Version: 1.0.2
 
 ## How to get and use?
 # git clone "https://github.com/hezhijie0327/GFWList2AGH.git" && chmod 0777 ./GFWList2AGH/release.sh && bash ./GFWList2AGH/release.sh
@@ -39,13 +39,9 @@ function OutputData() {
     gfwlist_data=($(awk "{ print $2 }" ./gfwlist_checked.tmp | sed '/\.$/d;/^!.*/d;/^$/d;/^@.*/d;/^[0-9\.]*$/d;/^[^\.]*$/d;/^\[.*/d' | sort | uniq))
     gfwlist_dns=(
         "https://doh.opendns.com:443/dns-query"
-        "tls://dns.google:853"
     )
     upstream_dns=(
         "https://dns.alidns.com:443/dns-query"
-        "https://dns.pub:443/dns-query"
-        "tls://dns.alidns.com:853"
-        "tls://dns.pub:853"
     )
     for upstream_dns_task in "${!upstream_dns[@]}"; do
         echo "${upstream_dns[$upstream_dns_task]}" >> ../upstream.txt
