b9cde338f6a2560de0975e27aad2afe5602b9d18
Zhijie He <hezhijie0327@hotmail.com>
2020-07-29 08:27:27 +0800

    release.sh | Version 1.1.1
    1. <Change> Change the value of "upstream_dns".

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Current Version: 1.1.0
+# Current Version: 1.1.1
 
 ## How to get and use?
 # git clone "https://github.com/hezhijie0327/GFWList2AGH.git" && chmod 0777 ./GFWList2AGH/release.sh && bash ./GFWList2AGH/release.sh
@@ -38,7 +38,7 @@ function OutputData() {
     )
     upstream_dns=(
         "https://dns.alidns.com:443/dns-query"
-        "https://dns.pub:443/dns-query"
+        "https://doh.pub:443/dns-query"
         "tls://dns.alidns.com:853"
         "tls://dns.pub:853"
     )
