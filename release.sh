38716e41dd1694d283021448a43e6be36cc32e36
Zhijie He <hezhijie0327@hotmail.com>
2020-08-01 09:23:48 +0800

    release.sh | Version 1.1.5
    1. <Change> Add support for CNACC.
    2. <Change> Exclude some rules from GFWList.

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Current Version: 1.1.4
+# Current Version: 1.1.5
 
 ## How to get and use?
 # git clone "https://github.com/hezhijie0327/GFWList2AGH.git" && chmod 0777 ./GFWList2AGH/release.sh && bash ./GFWList2AGH/release.sh
@@ -8,6 +8,11 @@
 ## Function
 # Get Data
 function GetData() {
+    cnacc_domain=(
+        "https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf"
+        "https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/apple.china.conf"
+        "https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/google.china.conf"
+    )
     gfwlist_base64=(
         "https://raw.githubusercontent.com/Loukky/gfwlist-by-loukky/master/gfwlist.txt"
         "https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt"
@@ -19,6 +24,9 @@ function GetData() {
         "https://raw.githubusercontent.com/pexcn/gfwlist-extras/master/gfwlist-extras.txt"
     )
     rm -rf ./*.txt ./*.yaml ./Temp && mkdir ./Temp && cd ./Temp
+    for cnacc_domain_task in "${!cnacc_domain[@]}"; do
+        curl -s --connect-timeout 15 "${cnacc_domain[$cnacc_domain_task]}" >> ./cnacc_domain.tmp
+    done
     for gfwlist_base64_task in "${!gfwlist_base64[@]}"; do
         curl -s --connect-timeout 15 "${gfwlist_base64[$gfwlist_base64_task]}" | base64 -d >> ./gfwlist_base64.tmp
     done
@@ -28,10 +36,15 @@ function GetData() {
 }
 # Analyse Data
 function AnalyseData() {
-    gfwlist_data=($(cat ./gfwlist_base64.tmp | grep -v "\!\|\%\|\*\|\/\|\@\|\[\|\]" | sed '/^\./d;s/\|//g' > ./gfwlist_data.tmp && cat ./gfwlist_domain.tmp | grep -v "\#" >> ./gfwlist_data.tmp && cat ./gfwlist_data.tmp | grep -v "[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}" | grep "\." | tr -d -c "[:alnum:]\-\.\n" | tr "A-Z" "a-z" | sed '/^$/d' | sort | uniq | awk "{ print $2 }"))
+    cnacc_data=($(cat ./cnacc_domain.tmp | grep -v "\#" | grep "server\=\/" | sed 's/\/114\.114\.114\.114//g;s/server\=\///g' > ./cnacc_data.tmp && cat ./cnacc_data.tmp | grep -v "[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}" | grep "\." | tr -d -c "[:alnum:]\-\.\n" | tr "A-Z" "a-z" | sed '/^$/d' | sort | uniq | awk "{ print $2 }"))
+    gfwlist_data=($(cat ./gfwlist_base64.tmp | grep -v "\!\|\%\|\*\|\/\|\@\|\[\|\]" | sed '/^\./d;s/\|//g' > ./gfwlist_data.tmp && cat ./gfwlist_domain.tmp | grep -v "\#" >> ./gfwlist_data.tmp && awk 'NR == FNR { tmp[$0] = 1 } NR > FNR { if ( tmp[$0] != 1 ) print }' ./cnacc_data.tmp ./gfwlist_data.tmp | grep -v "[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}" | grep "\." | tr -d -c "[:alnum:]\-\.\n" | tr "A-Z" "a-z" | sed '/^$/d' | sort | uniq | awk "{ print $2 }"))
 }
 # Output Data
 function OutputData() {
+    cnacc_dns=(
+        "https://dns.alidns.com:443/dns-query"
+        "tls://dns.alidns.com:853"
+    )
     gfwlist_dns=(
         "https://doh.opendns.com:443/dns-query"
         "tls://dns.google:853"
@@ -43,6 +56,12 @@ function OutputData() {
     for upstream_dns_task in "${!upstream_dns[@]}"; do
         echo "${upstream_dns[$upstream_dns_task]}" >> ../gfwlist2agh_web.txt
         echo "  - ${upstream_dns[$upstream_dns_task]}" >> ../gfwlist2agh_conf.yaml
+    done
+    for cnacc_dns_task in "${!cnacc_dns[@]}"; do
+        for cnacc_data_task in "${!cnacc_data[@]}"; do
+            echo "[/${cnacc_data[$cnacc_data_task]}/]${cnacc_dns[cnacc_dns_task]}" >> ../gfwlist2agh_web.txt
+            echo "  - '[/${cnacc_data[$cnacc_data_task]}/]${cnacc_dns[cnacc_dns_task]}'" >> ../gfwlist2agh_conf.yaml
+        done
     done
     for gfwlist_dns_task in "${!gfwlist_dns[@]}"; do
         for gfwlist_data_task in "${!gfwlist_data[@]}"; do
