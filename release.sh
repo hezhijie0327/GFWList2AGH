897a2b725cfba0e77505d2ea6be081415a6c00fb
Zhijie He <hezhijie0327@hotmail.com>
2020-08-11 16:33:20 +0800

    release.sh | Version 1.3.4
    1. <Change> Change the value of "upstream_dns" and "cnacc_dns" (DNSPod DoH and DoT).

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Current Version: 1.3.3
+# Current Version: 1.3.4
 
 ## How to get and use?
 # git clone "https://github.com/hezhijie0327/GFWList2AGH.git" && chmod 0777 ./GFWList2AGH/release.sh && bash ./GFWList2AGH/release.sh
@@ -51,7 +51,9 @@ function AnalyseData() {
 function OutputData() {
     upstream_dns=(
         "https://dns.alidns.com:443/dns-query"
+        "https://dns.pub:443/dns-query"
         "tls://dns.alidns.com:853"
+        "tls://dns.pub:853"
     )
     for upstream_dns_task in "${!upstream_dns[@]}"; do
         echo "${upstream_dns[$upstream_dns_task]}" >> ../gfwlist2agh_cnacc.txt
@@ -63,7 +65,9 @@ function OutputData() {
     done
     cnacc_dns=(
         "https://dns.alidns.com:443/dns-query"
+        "https://dns.pub:443/dns-query"
         "tls://dns.alidns.com:853"
+        "tls://dns.pub:853"
     )
     for cnacc_dns_task in "${!cnacc_dns[@]}"; do
         for cnacc_data_task in "${!cnacc_data[@]}"; do
