691d238ca330dfd3f7961cad1a03ed7e809a8599
Zhijie He <hezhijie0327@hotmail.com>
2020-12-15 10:45:16 +0800

    dnsproxy.sh | Version 1.0.4
    1. <Change> Add support to switch upstream file name.

--- dnsproxy.sh
+++ dnsproxy.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
-# Current Version: 1.0.3
+# Current Version: 1.0.4
 
 ## How to get and use?
 # git clone "https://github.com/hezhijie0327/GFWList2AGH.git" && bash ./GFWList2AGH/dnsproxy.sh
 
 ## Parameter
-while getopts b:c:e:f:h:k:l:m:p:q:r:t GetParameter; do
+while getopts b:c:e:f:h:k:l:m:p:q:r:t:v GetParameter; do
     case ${GetParameter} in
         b) BOOTSTRAP="${OPTARG}";;
         c) TLSCRT="${OPTARG}";;
@@ -20,6 +20,7 @@ while getopts b:c:e:f:h:k:l:m:p:q:r:t GetParameter; do
         q) QUICPORT="${OPTARG}";;
         r) RATELIMIT="${OPTARG}";;
         t) TLSPORT="${OPTARG}";;
+        v) VERSION="${OPTARG}";;
     esac
 done
 
@@ -56,7 +57,7 @@ function GenerateDefaultRuntimeScript() {
     echo "    --listen=${LISTEN:-0.0.0.0} --ratelimit=${RATELIMIT:-500}" '\' >> /etc/dnsproxy/conf/runtime.sh
     echo "    --cache-max-ttl=86400 --cache-min-ttl=10 --cache-size=65536" '\' >> /etc/dnsproxy/conf/runtime.sh
     echo "    --https-port=${HTTPSPORT:-0} --port=${PORT:-53} --quic-port=${QUICPORT:-0} --tls-port=${TLSPORT:-0}" '\' >> /etc/dnsproxy/conf/runtime.sh
-    wget -qO- "https://source.zhijie.online/GFWList2AGH/main/gfwlist2agh_combine_debug.txt" | sed "s/$/\ \\\/g;s/^/\ \ \ \ \-\-upstream\=/g" >> /etc/dnsproxy/conf/runtime.sh
+    wget -qO- "https://source.zhijie.online/GFWList2AGH/main/gfwlist2agh_${VERSION:-combine}.txt" | sed "s/$/\ \\\/g;s/^/\ \ \ \ \-\-upstream\=/g" >> /etc/dnsproxy/conf/runtime.sh
     echo "    --bootstrap=${BOOTSTRAP:-223.5.5.5:53} --fallback=${FALLBACK:-223.6.6.6:53}" '\' >> /etc/dnsproxy/conf/runtime.sh
 }
 # Generate Encrypt Runtime Script
