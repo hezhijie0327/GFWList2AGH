453650f1f1e42466601d021fa2b23573928d0ee1
Jimmyshjj <jimmyshjj@gmail.com>
2022-02-24 16:17:34 +0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -114,7 +114,7 @@ function GenerateRules() {
     case ${software_name} in
         adguardhome)
             domestic_dns=(
-                "https://dns.alidns.com/dns-query"
+                "tls://dns.alidns.com"
             )
             foreign_dns=(
                 "tls://dns.google
